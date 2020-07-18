1d47a65733d9af7540008b6fee8a9914db8a7784
wbbhacker <2404016117@qq.com>
2020-07-18 20:34:09 +0800

    webpack 开发环境配置

--- code/vue-app-base/webpack.dev.js
+++ code/vue-app-base/webpack.dev.js
@@ -1,5 +1,10 @@
 const path = require('path');
-
 module.exports = {
   mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    hot:true
+  }
 };
